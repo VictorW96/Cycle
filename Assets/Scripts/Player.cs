8dda5a3c214cdbaa3e0871028855e55e193eab1a
Victor Wolf <sWMAviwolf@stud.fh-rosenheim.de>
2020-05-21 19:43:08 +0200

    basic scene and start of movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MovingObject
+{
+    public int wallDamage = 1;
+    public int pointsPerFood = 10;
+    public int pointsPerSoda = 20;
+    public float restartLevelDelay = 1f;
+
+    private Animator animator;
+    private int food;
+
+    protected override void Start()
+    {
+        animator = GetComponent<Animator>();
+
+        food = GameManager.instance.playerFoodPoints;
+
+        base.Start();
+    }
+
+    private void OnDisable()
+    {
+        GameManager.instance.playerFoodPoints = food;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!GameManager.instance.playersTurn) return;
+
+        int horizontal = 0;
+        int vertical = 0;
+    }
+    private void CheckIfGameOver()
+    {
+        if (food <= 0)
+        {
+            GameManager.instance.GameOver();
+        }
+    }
+
+    protected override void AttemptMove<T>(int xDir, int yDir)
+    {
+        
+        food--;
+
+        base.AttemptMove<T>(xDir, yDir);
+        RaycastHit2D hit;
+        
+        CheckIfGameOver();
+        GameManager.instance.playersTurn = false;
+    }
+
+}
